d7e3fde46c3db72e1c38c2f02ef0dd83f43e1c7f
Konrad <konradceniuch@icloud.com>
2024-08-20 19:30:43 +0200

    prep

--- backend/src/main/java/com/ceniuch/backend/Repositories/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ceniuch.backend.Repositories;
-
-import com.ceniuch.backend.Entities.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Integer> {
-    Iterable<Message> findAllByChatId(int chatId);
-}
